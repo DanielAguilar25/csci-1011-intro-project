360e6026e50f958eb9639d34dd06f30aca566ca6
Danie Aguilar <da771200@gmail.com>
2022-11-22 13:22:02 -0600

    Add games data project

---
+++ games-data/games-released-birthyear-query.sql
@@ -0,0 +1,1 @@
+SELECT * FROM games WHERE first_release_date between '2003-01-01 00:00:00' AND '2003-10-21 23:59:59'ORDER BY name;
---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+--INSERT INTO sandbox(int64_value ,bool_value ,string_value) VALUES (67486730486730, 1, 'how does this work')
+
+--UPDATE sandbox SET double_value = -2.1543054 WHERE id = 125;
+
+--DELETE FROM sandbox WHERE id = 92
+
+SELECT * FROM sandbox;
